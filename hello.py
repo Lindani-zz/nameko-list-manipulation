9bcb11b8b0da2a26e9d1f535548d034980206e8f
Lindani <panilindani02@gmail.com>
2019-06-27 12:44:19 +0200

    encode a list of strings and return encoded dictionary

--- hello.py
+++ hello.py
@@ -1,18 +1,25 @@
 from nameko.rpc import rpc
+from dahuffman import HuffmanCodec
 import math
-
 
 class GreetingService:
     name = "greeting_service"
-
-    @rpc
-    def hello(self, name):
-        return "Hello, {}!".format(name)
 
     @rpc
     def squareOddNumbers(self, numbers = []):
         oddNumbers = list(filter(lambda number: number % 2, numbers))
         res = list(map(lambda oddNumber: math.sqrt(oddNumber), oddNumbers))
         return res
+
+    @rpc
+    def encodedStringDict(self, listOfStrings = []):
+        codec = HuffmanCodec.from_data(listOfStrings)
+        encoded = codec.encode(listOfStrings)
+        encodedDict = dict( zip(listOfStrings, encoded ))
+        return encodedDict
+
+
+
+    # ["hello", "at" , "test" , "this" , "here" , "now" ]
 
     # [1, 2, 3, 4, 5, 6, 9]
